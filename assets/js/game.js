e65ac6ca3c9766ae2165b0bb96e60159844be6cc
a.breezy <ambrose.wilkins@gmail.com>
2021-10-13 10:42:22 -0400

    add conditionals/control flow to game

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,14 +1,44 @@
-// creates a funtion named 'fight'
+var playerName = window.prompt("What's your robot's name?")
+var playerHealth = 100;
+var playerAttack = 10;
 
-function fight() {
-    window.alert("The fight has begun!");
+// you can log multiple things at once:
+console.log(playerName, playerHealth, playerAttack);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    // alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
+
+    // subtracts value of playerAttack from value of enemyHealth
+    enemyHealth = enemyHealth - playerAttack
+
+    // log resulting message to the console so that we know that it worked
+    console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.")
+
+    // check enemy's health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+    // subtracts value of enemyAttack from value of playerHealth
+    playerHealth = playerHealth - enemyAttack
+
+    // log resulting message to the console so that we know that it worked
+    console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.")
+
+    // check player's health
+    if(playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
 }
 
-var playerName = window.prompt("What is your robot's name?");
-// playerName is a var, not a string
-console.log(playerName);
-console.log("This logs a string, good for leaving yourself a message")
-//this will do math and log 20
-console.log(10 + 10)
-console.log("Our robot's name is" + playerName);
-// fight();
+fight();
